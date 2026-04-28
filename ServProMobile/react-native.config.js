6705e023997dc466bd3667fe2ea8929efebd5595
hechmi10 <hechmi.jaidane@gmail.com>
2026-04-28 12:43:04 +0100

    Fix language issue

---
+++ ServProMobile/react-native.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  dependencies: {
+    '@react-native-async-storage/async-storage': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+    'react-native-gesture-handler': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+    'react-native-reanimated': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+    'react-native-safe-area-context': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+    'react-native-screens': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+    'react-native-worklets': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+  },
+};
